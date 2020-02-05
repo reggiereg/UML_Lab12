85da1345e48873446642177f935137e5aa45491f
Reginald Richardson <youngreg21@gmail.com>
2020-02-05 08:05:33 -0500

    Submission for lab 12

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12_ClassUML
+{
+    class Person
+    {
+        private string name;
+        private string address;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Person(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+    }
+}
